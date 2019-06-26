1112d9648fb132fa5916666a6406d98df74582f6
Evolyuta <dima.97_97@mail.ru>
2019-06-26 11:36:42 +0300

    add files for libraries and configs

--- applications/applications.php
+++ applications/applications.php
@@ -1,39 +1,15 @@
 <?php
-/* Основные настройки */
+require "inc/lib.inc.php";
+require "inc/config.inc.php";
 
+$name = clearStr($_POST['name']);
+$address = clearStr($_POST['address']);
+$phone = clearStr($_POST['phone']);
+$email = clearStr($_POST['email']);
+$msg = clearStr($_POST['msg']);
+$date = time();
 
-const DB_HOST = 'localhost';
-const DB_LOGIN = 'root';
-const DB_PASSWORD = "";
-const DB_NAME = 'applications';
-
-$link = mysqli_connect(DB_HOST, DB_LOGIN, DB_PASSWORD, DB_NAME) or die(mysqli_connect_error());
-
-function clearStr($data)
-{
-    global $link;
-    $data = trim(strip_tags($data));
-    return mysqli_real_escape_string($link, $data);
-}
-
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $name = clearStr($_POST['name']);
-    $address = clearStr($_POST['address']);
-    $phone = clearStr($_POST['phone']);
-    $email = clearStr($_POST['email']);
-    $msg = clearStr($_POST['msg']);
-    $date = abs((int)(time()));
-
-    $sql = 'INSERT INTO applications (name, address, phone, email, msg, datetime) VALUES (?,?,?,?,?,?)';
-
-    if (!$stmt = mysqli_prepare($link, $sql)) return false;
-    mysqli_stmt_bind_param($stmt, "ssssss", $name, $address, $phone, $email, $msg, $date);
-    mysqli_stmt_execute($stmt);
-    mysqli_stmt_close($stmt);
-    header("Location: " . $_SERVER["REQUEST_URI"]);
-    exit;
-}
-
+addItemToCatalog($name, $address, $phone, $email, $msg, $date);
 
 ?>
 
