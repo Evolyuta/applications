69ab83b28a2c5571b306355afac1af1fca9a787b
Evolyuta <dima.97_97@mail.ru>
2019-06-26 11:51:54 +0300

    add index for admin

---
+++ applications/admin/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>Администрирование</title>
+    <meta charset="utf-8">
+</head>
+<body>
+<h1>Администрирование магазина</h1>
+<h3>Доступные действия:</h3>
+<ul>
+    <li><a href='applicationsList.php'>Просмотр готовых заказов</a></li>
+</ul>
+</body>
+</html>
