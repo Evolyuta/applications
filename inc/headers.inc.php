974cee48a66a126fa7320af9029698cd5dfa25ab
Evolyuta <dima.97_97@mail.ru>
2019-06-27 05:30:33 +0300

    hotfix

--- inc/headers.inc.php
+++ inc/headers.inc.php
@@ -8,18 +8,6 @@ switch ($id) {
         $title = 'Контакты';
         $header = 'Обратная связь';
         break;
-    case 'about':
-        $title = 'О нас';
-        $header = 'О нашем сайте';
-        break;
-    case 'info':
-        $title = 'Информация';
-        $header = 'Информация';
-        break;
-    case 'log':
-        $title = 'Журнал посещений';
-        $header = 'Журнал посещений';
-        break;
     case 'gbook':
         $title = 'Гостевая книга';
         $header = 'Наша гостевая книга';
