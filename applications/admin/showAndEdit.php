351f47c8eea5c1b293af7a0b4883b1b9bd77abf3
Evolyuta <dima.97_97@mail.ru>
2019-06-27 07:43:48 +0300

    hotfix

--- applications/admin/showAndEdit.php
+++ applications/admin/showAndEdit.php
@@ -21,18 +21,19 @@ if ($id) {
     <p><b>Дата размещения заявки</b>: <?= date("d-m-Y H:i", $client["datetime"]) ?></p>
     <p><b>Сообщение от клиента</b>: <?= $client["msg"] ?></p>
     <?
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        if ($_POST['name']) $name = clearStr($_POST['name']); else $name = $client["name"];
-        if ($_POST['address']) $address = clearStr($_POST['address']); else $address = $client["address"];
-        if ($_POST['phone']) $phone = clearStr($_POST['phone']); else $phone = $client["phone"];
-        if ($_POST['email']) $email = clearStr($_POST['email']); else $email = $client["email"];
-        if ($_POST['msg']) $msg = clearStr($_POST['msg']); else $msg = $client["msg"];
-        $date = $client["datetime"];
-        editItem($name, $address, $phone, $email, $msg, $date, $id);
 
-        echo "Заявка изменена! <a href='applicationsList.php'>Вернуться к списку заявок";
-        exit;
-    }
+}
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if ($_POST['name']) $name = clearStr($_POST['name']); else $name = $client["name"];
+    if ($_POST['address']) $address = clearStr($_POST['address']); else $address = $client["address"];
+    if ($_POST['phone']) $phone = clearStr($_POST['phone']); else $phone = $client["phone"];
+    if ($_POST['email']) $email = clearStr($_POST['email']); else $email = $client["email"];
+    if ($_POST['msg']) $msg = clearStr($_POST['msg']); else $msg = $client["msg"];
+    $date = $client["datetime"];
+    editItem($name, $address, $phone, $email, $msg, $date, $id);
+
+    echo "Заявка изменена! <a href='applicationsList.php'>Вернуться к списку заявок";
+    exit;
 }
 ?>
 
